0673c3c36c45f9057d723722899ab152bc8927f4
Thomas Rettig <thomas.rettig.11@gmail.com>
2023-08-20 23:50:02 +0800

    Update PWA

--- sw.js
+++ sw.js
@@ -5,6 +5,10 @@ var urls = [
   "fonts/MayenneSans-Regular-Web.woff2",
   "css/style.css",
   "images/favicon.svg",
+  "images/icon-192x192.png",
+  "images/icon-256x256.png",
+  "images/banner-600x380.png",
+  "images/banner2-600x380.png",
   "js/scripts.js",
   "js/word-data-set.js",
   "manifest.webmanifest"
