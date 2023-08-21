709f892c781fd9b4bbeec9f3869cf2fc87f554c6
Thomas Rettig <thomas.rettig.11@gmail.com>
2023-08-21 23:15:35 +0800

    Updates!

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,7 @@ localStorage.setItem("localColorArr", JSON.stringify(modifiedColors));
 // Change the background color
 document.body.style.backgroundColor = modifiedColors[0];
 
-// Sharing functionality
+// Copy to clipboard
 function copyToClipboard(elementId) {
   const element = document.getElementById(elementId);
   if (element) {
@@ -51,11 +51,26 @@ document.getElementById("copy").addEventListener("click", () => {
   copyToClipboard("word");
 });
 
+// Sharing functionality
+const response = await fetch(`window.location.hostname/?word=${wordName}`);
+  const blob = await response.blob();
+  const filesArray = [
+    new File(
+      [blob],
+      'wordsmith.png',
+      {
+        type: "image/png",
+        lastModified: new Date().getTime()
+      }
+   )
+  ];
+
 document.getElementById("share").addEventListener("click", () => {
   navigator.share({
     title: `New word — ${wordName}!`,
     text: `I learnt a new word called “${wordName}” today via Wordsmith, a web-based app that allows you to learn challenging vocabulary on the go!`,
-    url: window.location.href,
+    url: window.location.hostname,
+    files: filesArray,
   });
 });
 
---
+++ og/og.tsx
@@ -0,0 +1,53 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import React from "https://esm.sh/react@18.2.0";
+import { ImageResponse } from "https://deno.land/x/og_edge/mod.ts";
+
+const colorsArr: string[] = [
+  "#5F0B0D", "#5E2136", "#4F325C", "#214187", "#253764",
+  "#1F2942", "#0F151F", "#132521", "#16321F", "#183319",
+  "#1B383D", "#1E3C62", "#214186", "#362F5E", "#4A1D35"
+];
+
+function getRandomColor(): string {
+  const randomIndex = Math.floor(Math.random() * colorsArr.length);
+  return colorsArr[randomIndex];
+}
+
+const selectedColor: string = getRandomColor();
+
+export default async function handler(req: Request) {
+  const url = new URL(req.url);
+  const word = url.searchParams.get("word");
+
+  return new ImageResponse(
+    <div
+      style={{
+        fontSize: 60,
+        color: "white",
+        backgroundColor: selectedColor,
+        width: "100%",
+        height: "100%",
+        paddingTop: 50,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+      }}
+    >
+      <img
+        alt="avatar"
+        width="256"
+        src={"https://wordsmithe.vercel.app/images/favicon.svg"}
+      />
+      <h1>
+        {word}
+      </h1>
+    </div>,
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
+
+serve(handler);
