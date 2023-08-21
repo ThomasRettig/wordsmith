10ded55822b2701383d565386727245001eb25a2
Thomas Rettig <thomas.rettig.11@gmail.com>
2023-08-21 19:56:22 +0800

    Basic "share" functionality

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ var urls = [
   "/",
   "fonts/MayenneSans-Regular-Web.woff2",
   "css/style.css",
+  "images/share.svg",
   "images/favicon.svg",
   "images/icon-192x192.png",
   "images/icon-256x256.png",
