ec2b6b8560b4ddfd30b55d9d4618482076b6697f
Thomas Rettig <thomas.rettig.11@gmail.com>
2023-08-21 16:56:51 +0800

    Refactor Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,46 @@
-// Randomly select words from object and place into DOM
-const word = words[Math.floor(Math.random()*words.length)];
+// Randomly select a word
+const randomWord = words[Math.floor(Math.random() * words.length)];
 
-const wordName = word.word;
-const wordType = word.wordtype;
-const wordDefinition = word.definition;
-const wordSynonyms = word.synonyms;
+const {
+  word: wordName,
+  wordtype: wordType,
+  definition: wordDefinition,
+  synonyms: wordSynonyms,
+} = randomWord;
 
-document.querySelector('.js-word').innerHTML = wordName;
-document.querySelector('.js-type').innerHTML = wordType;
-document.querySelector('.js-definition').innerHTML = wordDefinition;
-document.querySelector('.js-synonyms').innerHTML = wordSynonyms;
-document.querySelector('.js-dict-link').setAttribute('href', `https://www.merriam-webster.com/dictionary/${wordName}`);
+document.querySelector(".js-word").innerHTML = wordName;
+document.querySelector(".js-type").innerHTML = wordType;
+document.querySelector(".js-definition").innerHTML = wordDefinition;
+document.querySelector(".js-synonyms").innerHTML = wordSynonyms;
+document.querySelector(".js-dict-link").setAttribute("href", `https://www.merriam-webster.com/dictionary/${wordName}`);
 
-// modify array
-function nextVal(arr) {
-	firstElm = arr.shift();
-	arr.push(firstElm);
-	backgroundColorDelta(firstElm);
-	return arr;
+// Rotate the colors array
+function rotateArray(arr) {
+  const [firstElement, ...rest] = arr;
+  arr.push(firstElement);
+  return rest;
 }
 
-// set background color
-function backgroundColorDelta (firstElm) {
-	document.getElementsByTagName('body')[0].style.backgroundColor = firstElm;
-}
+const colorsArr = [
+  "#5F0B0D", "#5E2136", "#4F325C", "#214187", "#253764",
+  "#1F2942", "#0F151F", "#132521", "#16321F", "#183319",
+  "#1B383D", "#1E3C62", "#214186", "#362F5E", "#4A1D35"
+];
 
-if (localStorage.getItem('localColorArr') === null) {
-	//set array first time if it doesn't exist
-	var colorsArr = ['#5F0B0D', '#5E2136', '#4F325C', '#214187', '#253764', '#1F2942', '#0F151F', '#132521', '#16321F', '#183319', '#1B383D', '#1E3C62', '#214186', '#362F5E', '#4A1D35'];
-	localStorage.setItem('localColorArr', JSON.stringify(colorsArr));
-	var localColorArr = JSON.parse(localStorage.getItem('localColorArr'));
-} else {
-	var localColorArr = JSON.parse(localStorage.getItem('localColorArr'));
-	//modify array
-	modifiedColors = nextVal(localColorArr);
-	// set setLocalStorage = modifiedArray;
-	localStorage.setItem('localColorArr', JSON.stringify(modifiedColors));
-}
+const localColorArr = JSON.parse(localStorage.getItem("localColorArr")) || colorsArr;
+const modifiedColors = rotateArray(localColorArr);
+localStorage.setItem("localColorArr", JSON.stringify(modifiedColors));
 
-// handle refreshing
+// Change the background color
+document.body.style.backgroundColor = modifiedColors[0];
 
-document.addEventListener('keyup', () => {
-  if (event.code === 'Space') {
+// Handle refreshing
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
     window.location.reload();
   }
-})
+});
 
-document.getElementById('reload').addEventListener('click', () => {
+document.getElementById("reload").addEventListener("click", () => {
   window.location.reload();
-})
+});
